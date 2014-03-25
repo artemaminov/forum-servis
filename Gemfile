39580245038ec9fa4d40b84c18fe7f310d09de70
Artem Aminov <artemaminov@gmail.com>
2014-03-25 11:29:49 +0600

    Add SimpleRSS

--- Gemfile
+++ Gemfile
@@ -23,6 +23,8 @@ end
 
 gem 'jquery-rails'
 
+gem 'simple-rss'
+
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
 
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,16 @@
 class HomeController < ApplicationController
   def index
+    require 'rubygems'
+    require 'simple-rss'
+    require 'open-uri'
+
+    SimpleRSS.feed_tags << :"yandex:full-text"
+    SimpleRSS.item_tags << :"yandex:full-text"
+    rss = SimpleRSS.parse open('http://rosan.com/data/rss/37/')
+    @brp_news = rss.items
+
+    #rss.channel.title # => "Slashdot"
+    #rss.channel.link # => "http://slashdot.org/"
+    #rss.items.first.link # => "http://books.slashdot.org/article.pl?sid=05/08/29/1319236&amp;from=rss"
   end
 end
