a6cec0f7649cf9293eaf002d77625da4a92b6b78
Artem Aminov <artemaminov@gmail.com>
2014-03-26 14:52:02 +0600

    Add Headroom.js and scrollTo function

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -3,4 +3,33 @@ $(document).ready(function () {
         responsive: true
     });
 
+    $(".up").headroom({
+        // vertical offset in px before element is first unpinned
+        offset : 600,
+        // scroll tolerance in px before state changes
+        tolerance : 100,
+        // css classes to apply
+        classes : {
+            // when element is initialised
+            initial : "",
+            // when scrolling up
+            pinned : "",
+            // when scrolling down
+            unpinned : "",
+            // when above offset
+            top : "fadeOutDown",
+            // when below offset
+            notTop : "fadeInUp"
+        }
+    });
+
 });
+
+function scrollTo(selector) {
+    if (selector == '#top') {
+        margin = 0
+    } else {
+        margin = 20
+    }
+    $('html, body').animate({ scrollTop:  $(selector).offset().top + margin }, 'slow');
+}
