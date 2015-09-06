c79cac406ceb6dcf27277145100d180945fd0e80
Artem Aminov <artemaminov@gmail.com>
2015-09-06 14:04:45 +0500

    clean assets

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,8 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require jquery
-//= require jquery_ujs
-//= require_tree .
+//= require jquery.min
+//= require jquery.headroom.min
+//= require jquery.carouFredSel-6.2.1-packed
+//= require headroom.min
+//= require script
--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -6,26 +6,17 @@ $(document).ready(function () {
         responsive: true
     });
 
-    $(".up").headroom({
-        // vertical offset in px before element is first unpinned
-        offset: 600,
-        // scroll tolerance in px before state changes
+    $('.up').headroom({
+        offset: 300,
         tolerance: 100,
-        // css classes to apply
         classes: {
-            // when element is initialised
             initial: "",
-            // when scrolling up
             pinned: "",
-            // when scrolling down
             unpinned: "",
-            // when above offset
             top: "fadeOutDown",
-            // when below offset
             notTop: "fadeInUp"
         }
     });
-
 });
 
 function scrollTo(selector) {
