6dfeb151200d50113665e17bcf2088c1524be9dc
artemaminov <artemaminov@gmail.com>
2017-04-22 17:10:10 +0500

    remove rosan news

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,6 @@ class HomeController < ApplicationController
   def index
 
     Feedjira::Feed.add_common_feed_entry_element('yandex:full_text', :as => :yandex)
-    @brp_news = Feedjira::Feed.fetch_and_parse("http://rosan.com/data/rss/37/")
     @banners = Carousel.all
 
   end
